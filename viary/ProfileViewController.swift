030381d6628c85654dce48b0ea82971472945d1c
Andy Chen <andychen@10.0.2.15>
2021-12-09 05:52:36 -0800

    Profile

---
+++ viary/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  viary
+//
+//  Created by Andy Chen on 09/12/21.
+//  Copyright © 2021 ltm. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+   
+    @IBOutlet weak var PUsername: UITextField!
+    @IBOutlet weak var PEmail: UITextField!
+    @IBOutlet weak var PDoB: UITextField!
+    @IBOutlet weak var PHobby: UITextField!
+    @IBOutlet weak var PDream: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
