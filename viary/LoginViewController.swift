a86cc26c8ce7eb6c3bf45fd2d815d8ad84e07343
owenltm <owenlwiantoro01@gmail.com>
2021-12-16 15:14:19 +0800

    login register finish

--- viary/LoginViewController.swift
+++ viary/LoginViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
+    @IBAction func login(_ sender: Any) {
+        var valid = true
+        
+        if Helper.numberOfCharacters(str: usernameTextField.text!) == 0 || Helper.numberOfCharacters(str: passwordTextField.text!) == 0 {
+            valid = false
+        }
+        
+        if valid {
+            var authenticated = false
+            
+            //Check user data
+            authenticated = true
+            
+            if authenticated {
+                performSegue(withIdentifier: "goToMain", sender: self)
+            }
+        }
+    }
 }
