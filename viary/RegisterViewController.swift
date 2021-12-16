1656e77d21d55b2cdbef50c0c485d4a2368f5592
owenltm <owenlwiantoro01@gmail.com>
2021-12-16 14:04:11 +0800

    init welcome, register, and login screen

---
+++ viary/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  viary
+//
+//  Created by user on 16/12/21.
+//  Copyright © 2021 ltm. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
